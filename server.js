666fb90f6567b7cb28bfd1b9ad7fd02b5482a92d
Akinde Tosin Michael <tosin.akinde36@gmail.com>
2024-12-22 23:00:43 +0100

    server.js update

--- server.js
+++ server.js
@@ -14,30 +14,6 @@ connectToMongodb();
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
 
-// Export the app for Vercel
-module.exports = app;
-
-
-
-
-
-// const express = require("express");
-// const connectToMongodb = require("./config/db.js");
-// const authRoutes = require("./routes/authRoutes.js");
-// const blogRoutes = require("./routes/blogRoutes.js");
-// require("dotenv").config();
-
-// const app = express();
-// app.use(express.json());
-
-// // Connect to MongoDB
-//  connectToMongodb()
-
-
-// // Routes
-// app.use("/api/auth", authRoutes);
-// app.use("/api/blogs", blogRoutes);
-
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`)); // Fixed string interpolation
